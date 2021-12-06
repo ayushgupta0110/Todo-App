a913261c01e48a353b266eb0d0446ab140fa0460
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-12-06 18:02:57 +0530

    Added comments in app.py file

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ class Todo(db.Model):
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
 
+#Home Page route containing a list of methods available
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method=='POST':
@@ -28,12 +29,14 @@ def hello_world():
     allTodo = Todo.query.all() 
     return render_template('index.html', allTodo=allTodo)
 
+# route made for a /show
 @app.route('/show')
 def products():
     allTodo = Todo.query.all()
     print(allTodo)
-    return 'this is products page'
+    return 'If you want to see the list of all the todos, you can go to terminal.'
 
+# route made to update a specific todo based on the sno
 @app.route('/update/<int:sno>', methods=['GET', 'POST'])
 def update(sno):
     if request.method=='POST':
@@ -49,6 +52,7 @@ def update(sno):
     todo = Todo.query.filter_by(sno=sno).first()
     return render_template('update.html', todo=todo)
 
+# route made to delete a specific todo based on the sno
 @app.route('/delete/<int:sno>')
 def delete(sno):
     todo = Todo.query.filter_by(sno=sno).first()
@@ -56,5 +60,6 @@ def delete(sno):
     db.session.commit()
     return redirect("/")
 
+# calling the app on Port 8000
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
